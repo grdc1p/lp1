43a4d64ff844776911b0430f82ecb344a11752c1
grdc1p <gabrielcarvalhogrdc@gmail.com>
2024-09-11 13:04:10 -0300

    jogo

---
+++ script.js
@@ -0,0 +1,16 @@
+function gerarNumeroAleatorio() {
+const numeroAleatorio = (math.random() * 11)
+return numeroAleatorio   
+}
+
+function jogar () {
+const pegaNumeroAleatorio = gerarNumeroAleatorio()
+console.log(pegaNumeroAleatorio)
+do{
+    const palpite = prompt ("adivinhe um numero")
+}
+while()
+}
+
+jogar()
+
